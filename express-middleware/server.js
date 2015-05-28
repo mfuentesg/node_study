037c8dd8a3624280c2f370fa358b83a363f7494d
Marcelo Fuentes <marcelo@classroom.tv>
2015-05-28 09:59:16 -0400

    add param middleware to catch routes that have the same parameter names

--- express-middleware/server.js
+++ express-middleware/server.js
@@ -18,4 +18,23 @@ app.get('/data', function(request, response) {
     }
 });
 
+// Routes with the same paramater name
+app.param('name', function(request, response, next) {
+    request.upperName = request.params.name.toString().toUpperCase();
+
+    next();
+});
+
+app.get('/param1/:name', function(request, response) {
+    var name = request.upperName;
+
+    response.send(name);
+});
+
+app.get('/param2/:name', function(request, response) {
+    var name = request.upperName;
+
+    response.send(name);
+});
+
 app.listen(3000);
