68683d25d2b2405c1dd3e5540566a4e944e5be57
Marcelo Fuentes Gonzalez <marceloe.fuentes@gmail.com>
2015-05-31 14:29:13 -0400

    upload files with multer

--- upload-file/server-multer.js
+++ upload-file/server-multer.js
@@ -1,3 +1,20 @@
 var express = require('express'),
     app = express(),
+    multer = require('multer'),
     fs = require('fs');
+
+app.use(multer({
+    dest: './uploads-multer',
+    onFileUploadComplete: function(file, request, response) {
+        response.send('Upload Complete!');
+    }
+}));
+
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/index.html');
+});
+app.post('/', function(request, response) {});
+
+app.listen(3000, function() {
+    console.log('Server listening on port 3000');
+});
